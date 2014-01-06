448f107c07af3a8f54fd00c6c9a3438d1085fd31
hak8or <hak8or@gmail.com>
2014-01-06 02:49:53 -0500

    Steps are now logged.

--- config.sh
+++ config.sh
@@ -32,23 +32,29 @@ echo "| minutes.                                           |"
 echo "+----------------------------------------------------+"
 
 working_directory="$HOME"
+log_location="$working_directory/config.log"
 ruby_version=2.1.0
 
-# Add in the latest postgresql official ppa.
-        echo "  [1/10] Adding in the postgresql official PPA"
-        touch /etc/apt/sources.list.d/pgdg.list
+# Make the logging file
+    mkdir -p $working_directory
+    touch $log_location &>>/dev/null
 
-        cat <<- _EOF_ >/etc/apt/sources.list.d/pgdg.list
-            deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main
+# Add in the latest postgresql official ppa.
+    echo "  [1/10] Adding in the postgresql official PPA"
+    echo "----FROM SCRIPT ECHO---- Adding in the postgresql official PPA" &>>$log_location
+    touch /etc/apt/sources.list.d/pgdg.list &>>$log_location
+
+    cat <<- _EOF_ >/etc/apt/sources.list.d/pgdg.list
+        deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main
 _EOF_
 
-        wget http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc &>/dev/null
-        apt-key add ACCC4CF8.asc &>>bootstrap.log
+    wget http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc &>/dev/null
+    apt-key add ACCC4CF8.asc &>>$log_location
 
 # Lets fetch all the required dependencies.
-        echo "  [2/10] Updating ubuntu"
-        touch $HOME/bootstrap.log &>>bootstrap.log
-        apt-get update > /dev/null
+    echo "  [2/10] Updating ubuntu"
+    echo "----FROM SCRIPT ECHO---- Updating ubuntu" &>>$log_location
+    apt-get update > /dev/null
 
 # htop                          - A far better alternative to top to display current system usage statistics.
 # build-essential               - Used to compile ruby from the source.
@@ -58,75 +64,79 @@ _EOF_
 # libsqlite3-dev, sqlite3       - Required to run rails server.
 # openssl, libssl-dev           - Required for the rails server and bundle.
 # libcurl4-openssl-dev          - For ngnix
-        echo "  [3/10] Installing required packages"
-        echo "    |- [1/7] htop for your system statistics pleasures"
-		echo "======== htop for your system statistics pleasures" &>>bootstrap.log
-        apt-get install -y htop &>>bootstrap.log
+    echo "  [3/10] Installing required packages"
+    echo "    |- [1/7] htop for your system statistics pleasures"
+	echo "----FROM SCRIPT ECHO---- htop for your system statistics pleasures" &>>$log_location
+    apt-get install -y htop &>>$log_location
 
-        echo "    |- [2/7] build-essential used to compile ruby from source"
-		echo "======== build-essential used to compile ruby from the source." &>>bootstrap.log
-        apt-get install -y build-essential &>>bootstrap.log
+    echo "    |- [2/7] build-essential used to compile ruby from source"
+	echo "----FROM SCRIPT ECHO---- build-essential used to compile ruby from the source." &>>$log_location
+    apt-get install -y build-essential &>>$log_location
 
-        echo "    |- [3/7] openssl + libssl-dev for rails server and bundle"
-		echo "======== openssl + libssl-dev required for the rails server and bundle." &>>bootstrap.log
-        apt-get install -y openssl libssl-dev &>>bootstrap.log
+    echo "    |- [3/7] openssl + libssl-dev for rails server and bundle"
+	echo "----FROM SCRIPT ECHO---- openssl + libssl-dev required for the rails server and bundle." &>>$log_location
+    apt-get install -y openssl libssl-dev &>>$log_location
 
-        # echo "  |- [4/9] ruby-dev for rails and ngnix."
-		# This is not needed for ruby itself.
-        # apt-get install -y ruby-dev &>>bootstrap.log
+    # echo "  |- [4/9] ruby-dev for rails and ngnix."
+	# This is not needed for ruby itself.
+    # apt-get install -y ruby-dev &>>$log_location
 
-        echo "    |- [4/7] libsqlite3-dev + sqlite3 for running rails server"
-		echo "======== libsqlite3-dev + sqlite3 required to run rails server." &>>bootstrap.log
-        apt-get install -y libsqlite3-dev sqlite3 &>>bootstrap.log
+    echo "    |- [4/7] libsqlite3-dev + sqlite3 for running rails server"
+	echo "----FROM SCRIPT ECHO---- libsqlite3-dev + sqlite3 required to run rails server." &>>$log_location
+    apt-get install -y libsqlite3-dev sqlite3 &>>$log_location
 
-        # echo "  |- [6/9] nodejs Javascript runtime required for the rails asset pipeline."
-        # apt-get install -y nodejs &>>bootstrap.log
+    # echo "  |- [6/9] nodejs Javascript runtime required for the rails asset pipeline."
+    # apt-get install -y nodejs &>>$log_location
 
-        echo "    |- [5/7] zlib1g-dev for ngnix"
-		echo "======== zlib1g-dev for ngnix."  &>>bootstrap.log
-        apt-get install -y zlib1g-dev &>>bootstrap.log
+    echo "    |- [5/7] zlib1g-dev for ngnix"
+	echo "----FROM SCRIPT ECHO---- zlib1g-dev for ngnix."  &>>$log_location
+    apt-get install -y zlib1g-dev &>>$log_location
 
-        echo "    |- [6/7] libcurl4-openssl-dev for ngnix"
-		echo "======== libcurl4-openssl-dev for ngnix" &>>bootstrap.log
-        apt-get install -y libcurl4-openssl-dev &>>bootstrap.log
+    echo "    |- [6/7] libcurl4-openssl-dev for ngnix"
+	echo "----FROM SCRIPT ECHO---- libcurl4-openssl-dev for ngnix" &>>$log_location
+    apt-get install -y libcurl4-openssl-dev &>>$log_location
 
-        echo "    \- [7/7] postgresql-9.3 as a database server"
-		echo "======== postgresql-9.3 as a database server" &>>bootstrap.log
-        apt-get install -y libpq-dev &>>bootstrap.log
-        # This automatically installs postgresql and postgresql client!
-        apt-get install -y postgresql-9.3 &>>bootstrap.log
+    echo "    \- [7/7] postgresql-9.3 as a database server"
+	echo "----FROM SCRIPT ECHO---- postgresql-9.3 as a database server" &>>$log_location
+    apt-get install -y libpq-dev &>>$log_location
+    # This automatically installs postgresql and postgresql client!
+    apt-get install -y postgresql-9.3 &>>$log_location
 
 
 # Get ruby 2.1.0 stable source from the official ruby website.
     echo "  [4/10] Installing ruby"
     echo "    |- [1/5] Downloading ruby $ruby_version source tarball"
-    mkdir $working_directory
+    echo "----FROM SCRIPT ECHO---- Downloading ruby $ruby_version source tarball" &>>$log_location
     cd $working_directory
     wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-$ruby_version.tar.gz &>/dev/null
 
 # Extract and enter the resulting directory.
     echo "    |- [2/5] Extracting ruby source"
+    echo "----FROM SCRIPT ECHO---- Extracting ruby source" &>>$log_location
     tar -xzvf ruby-$ruby_version.tar.gz &>>/dev/null
-    cd ruby-$ruby_version &>>bootstrap.log
+    cd ruby-$ruby_version &>>$log_location
 
 # Install ruby from source.
     Processor_Count=`grep -c ^processor /proc/cpuinfo`
     echo "    |- [3/5] running configure"
-    ./configure  &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- running configure" &>>$log_location
+    ./configure  &>>$log_location
 
     echo "    |- [4/5] running make on $Processor_Count core(s). (This takes a while)"
-    make -j $Processor_Count &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- running make on $Processor_Count core(s). (This takes a while)" &>>$log_location
+    make -j $Processor_Count &>>$log_location
 
 	# Not sure if I actually need this for anything.
     # make test 
 
     echo "    \- [5/5] running install"
-    make install &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- running install" &>>$log_location
+    make install &>>$log_location
     cd ..
 
 # Clean the ruby installation files
-    rm -r -f $ruby_version &>>bootstrap.log
-    rm ruby-$ruby_version.tar.gz &>>bootstrap.log
+    rm -r -f $ruby_version &>>$log_location
+    rm ruby-$ruby_version.tar.gz &>>$log_location
     cd $working_directory
 		
 #----------------------- STOP ------------------------------
@@ -134,14 +144,17 @@ _EOF_
         # read -p "Press [Enter] key to continue..."
 #----------------------- STOP ------------------------------
 echo "  [5/10] Install remainder to stack."
+echo "----FROM SCRIPT ECHO---- Install remainder to stack." &>>$log_location
 
 # Mainly to update Rdoc and minitest and whatnot.
     echo "    |- [1/6] Updating ruby system gems"
-    gem update --system &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- Updating ruby system gems" &>>$log_location
+    gem update --system &>>$log_location
 
 # Installs rails via the gem. This could take a while.
     echo "    |- [2/6] Installing Rails (this takes a while too)"
-    gem install rails &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- Installing Rails (this takes a while too)" &>>$log_location
+    gem install rails &>>$log_location
 
 #----------------------- STOP ------------------------------
 		# echo "Stopped before setting up passenger and pg gem"
@@ -150,41 +163,47 @@ echo "  [5/10] Install remainder to stack."
 
 # Install passenger for ngnix which will act as a webserver for rails.
     echo "    |- [3/6] Installing passenger"
+    echo "----FROM SCRIPT ECHO---- Installing passenger" &>>$log_location
     # From here: http://www.modrails.com/documentation/Users%20guide%20Nginx.html#install_on_debian_ubuntu
     # These are daily builds from an official repo handled by Phusion people, so guaranteed to be up to date.
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7 &>>bootstrap.log
-    sudo apt-get install apt-transport-https &>>bootstrap.log
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7 &>>$log_location
+    sudo apt-get install apt-transport-https &>>$log_location
 
     cat <<- _EOF_ >/etc/apt/sources.list.d/passenger.list
                 deb https://oss-binaries.phusionpassenger.com/apt/passenger saucy main
 _EOF_
 
-    sudo chown root: /etc/apt/sources.list.d/passenger.list &>>bootstrap.log
-    sudo chmod 600 /etc/apt/sources.list.d/passenger.list &>>bootstrap.log
+    sudo chown root: /etc/apt/sources.list.d/passenger.list &>>$log_location
+    sudo chmod 600 /etc/apt/sources.list.d/passenger.list &>>$log_location
     sudo apt-get update &>>/dev/null
-    sudo apt-get -y install nginx-extras passenger &>>bootstrap.log
+    sudo apt-get -y install nginx-extras passenger &>>$log_location
 
 # Changes the swapfile from the standard 512MB to 1GB for ngnix installation. If
 # there is less than 1 GB of ram on the system so nginx won't complain.
     memory_size_KB=$(grep MemTotal /proc/meminfo | awk '{print $2}'  )
     if [ $memory_size_KB -lt 1048576 ]; then
         echo "    |- [4/6] Changing swap file size to 1 GB"
-        dd if=/dev/zero of=/swap bs=1M count=1024 &>>bootstrap.log
-        mkswap /swap &>>bootstrap.log
-        swapon /swap &>>bootstrap.log
+        echo "----FROM SCRIPT ECHO---- Changing swap file size to 1 GB" &>>$log_location
+        dd if=/dev/zero of=/swap bs=1M count=1024 &>>$log_location
+        mkswap /swap &>>$log_location
+        swapon /swap &>>$log_location
     else
         echo "    |- [4/6] Not changing swap file size to 1 GB"
+        echo "----FROM SCRIPT ECHO---- Not changing swap file size to 1 GB" &>>$log_location
     fi
 		
 # Installs the json and pg gems since they have issues when installed via rails new and then bundle.
     echo "    |- [5/6] Installing json gem"
-    gem install json &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- Installing json gem" &>>$log_location
+    gem install json &>>$log_location
 
     echo "    \- [6/6] Installing pg gem"
-    gem install pg &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- Installing pg gem" &>>$log_location
+    gem install pg &>>$log_location
 
 # Sets up a demo rails application and enter the directory.
     echo "  [6/10] Generating rails app"
+    echo "----FROM SCRIPT ECHO---- Generating rails app" &>>$log_location
 
     if [ -d "demo_rails_app" ]; then
       echo "        There is an old demo_rails_app directory here for some reason."
@@ -192,7 +211,7 @@ _EOF_
       mv demo_rails_app demo_rails_app.old
     fi
 
-    rails new demo_rails_app -d postgresql &>>bootstrap.log
+    rails new demo_rails_app -d postgresql &>>$log_location
 
     touch $working_directory/demo_rails_app/log/nginx_error.log
 
@@ -206,7 +225,8 @@ _EOF_
 # Run bundle so modifications to the gemfile get put into effect.
 	cd demo_rails_app
     echo "  [7/10] Running bundle"
-	sudo bundle &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- Running bundle" &>>$log_location
+	sudo bundle &>>$log_location
 	
 #----------------------- STOP ------------------------------
 		# echo "Stopped before setting up configurations."
@@ -215,6 +235,7 @@ _EOF_
 
 # Configure postgresql and set up users for it. Huge PITA to do this manually and remember it.
     echo "  [8/10] Configuring Postgres"
+    echo "----FROM SCRIPT ECHO---- Configuring Postgres" &>>$log_location
 
     user_name=demo_user
     password=pass1
@@ -230,6 +251,7 @@ _EOF_
 
 # Modify nginx.conf as much as I can via the script so that it uses ruby and RoR.
     echo "  [9/10] Editing nginx.conf"
+    echo "----FROM SCRIPT ECHO---- Editing nginx.conf" &>>$log_location
 
     # Make a backup of the original nginx.conf
     sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
@@ -246,7 +268,8 @@ _EOF_
 
 # Start the nginx server
     echo "  [10/10] Restarting the nginx server"
-    sudo service nginx restart &>>bootstrap.log
+    echo "----FROM SCRIPT ECHO---- Restarting the nginx server" &>>$log_location
+    sudo service nginx restart &>>$log_location
 
 Current_IP=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 rails_version=$(rails -v)
@@ -266,7 +289,7 @@ echo "| demo_rails_app_app                                           |"
 echo "|                                                              |"
 echo "| Demo RoR project located in $working_directory/demo_rails_app      |"
 echo "| Nginx error logs located in $working_directory/demo_rails_app/logs |"
-echo "| Log for this script located in $working_directory/bootstrap.log    |"
+echo "| Log for this script located in $working_directory/$log_location    |"
 echo "|                                                              |"
 echo "| Postgres Version: 9.3    Phusion Passenger version: 4.0.33   |"
 echo "| Ruby version: $ruby_version      Rails version: $rails_version          |"
